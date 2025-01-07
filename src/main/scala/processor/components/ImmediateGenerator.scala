57334d01006a2e21801c67a8e629d5bf1640c772
Hatem1498 <74409586+Hatem1498@users.noreply.github.com>
2025-01-07 14:07:40 +0100

    ImmediateGenerator done

--- src/main/scala/processor/components/ImmediateGenerator.scala
+++ src/main/scala/processor/components/ImmediateGenerator.scala
@@ -8,6 +8,25 @@ class ImmediateGenerator extends Module {
     val immediate = Output(SInt(32.W))
   })
 
-  
+  switch(io.instrType){
+    is(InstrType.RType.id.U){
+      io.immediate := 0.U
+    }
+    is(InstrType.IType.id.U){
+      io.immediate := io.instruction(31, 20).asSInt
+    }
+    is(InstrType.SType.id.U){
+      io.immediate := Cat(io.instruction(31, 25), io.instruction(11, 7)).asSInt
+    }
+    is(InstrType.BType.id.U){
+      io.immediate := Cat(io.instruction(31), io.instruction(7), io.instruction(30, 25), io.instruction(11, 8), 0.U(1.W)).asSInt
+    }
+    is(InstrType.UType.id.U){
+      io.immediate := Cat(io.instruction(31, 12), 0.U(12.W)).asSInt
+    }
+    is(InstrType.JType.id.U){
+      io.immediate := Cat(io.instruction(31), io.instruction(19, 12), io.instruction(20), io.instruction(30, 21), 0.U(1.W)).asSInt
+    }
+  }
 
 }
