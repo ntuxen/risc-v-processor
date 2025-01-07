c59efe63d14dc58426ccdae34d7513029e6c16d6
FrederikOeLar <184511953+FrederikOeLar@users.noreply.github.com>
2025-01-07 13:39:20 +0100

    Added ALU functions

--- src/main/scala/processor/components/ALU.scala
+++ src/main/scala/processor/components/ALU.scala
@@ -5,8 +5,26 @@ import chisel3.util._
 class ALU extends Module {
   val io = IO(new Bundle {
     val ALUSel = Input(UInt(8.W))
-    val operand1 = Input(UInt(32.W))
-    val operand2 = Input(UInt(32.W))
+    val operand1 = Input(SInt(32.W))
+    val operand2 = Input(SInt(32.W))
     val result = Output(UInt(32.W))
   })
+
+  switch(io.ALUSel){
+    is(AluOperation.Add.id.U){
+      io.result := io.operand1 + io.operand2
+    }
+    is(AluOperation.Sub.id.U){
+      io.result := io.operand1 - io.operand2
+    }
+    is(AluOperation.And.id.U){
+      io.result := io.operand1 & io.operand2
+    }
+    is(AluOperation.Or.id.U){
+      io.result := io.operand1 | io.operand2
+    }
+    is(AluOperation.Xor.id.U){
+      io.result := io.operand1 ^ io.operand2
+    }
+  }
 }
