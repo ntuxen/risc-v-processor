a324fc0dac7fd04a4dcfdff51213135fa26db352
Hatem1498 <74409586+Hatem1498@users.noreply.github.com>
2025-01-10 14:35:21 +0100

    Small changes

--- src/test/scala/ProcessorTopLevelTester.scala
+++ src/test/scala/ProcessorTopLevelTester.scala
@@ -2,26 +2,17 @@ import chisel3._
 import chiseltest._
 import org.scalatest.flatspec.AnyFlatSpec
 import processor.ProcessorTopLevel
+import processor.components.ProgramLoader
 
 class ProcessorTopLevelTester extends AnyFlatSpec with ChiselScalatestTester {
   // Define the program
-  val program: Seq[UInt] = Seq(
-    "h03700193".U(32.W), // Sample instructions
-    "h00600093".U(32.W),
-    "h00700113".U(32.W),
-    "h00000013".U(32.W),
-    "h00000013".U(32.W),
-    "h00000013".U(32.W),
-    "h00000013".U(32.W),
-    "h0020a1b3".U(32.W),
-    "h0030b213".U(32.W)
-  )
+  val program: Seq[UInt] = ProgramLoader.loadHexFile("src/test/TestPrograms/Beq_Bne_Test.hex")
 
   "ProcessorTopLevelTester" should "pass" in {
     test(new ProcessorTopLevel(program)).withAnnotations(Seq(WriteVcdAnnotation))
     { dut =>
 
-      dut.clock.step(20)
+      dut.clock.step(100)
 
     }
   }
