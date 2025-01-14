5d801ee252f559deee8a4e74a757b69762e3d9e1
FrederikOeLar <184511953+FrederikOeLar@users.noreply.github.com>
2025-01-14 15:36:23 +0100

    Broken stuff

--- src/main/scala/processor/ProcessorTopLevel.scala
+++ src/main/scala/processor/ProcessorTopLevel.scala
@@ -11,53 +11,59 @@ class ProcessorTopLevel(val program: Seq[UInt]) extends Module {
     val LEDs = Output(UInt(16.W))
   })
 
-  val IF = Module(new IFDStage(program))
-  val ID = Module(new InstructionDecoder) // ID Stage
+  val IFD = Module(new IFDStage(program))
+  //val ID = Module(new InstructionDecoder) // ID Stage
   val EX = Module(new EXStage)
   val MEM = Module (new MEMStage)
+  val WB = Module (new WBStage)
 
   //-------------Connections-------------//
   //---- ID <- IF -----//
   // There are no input register in the ID stage; instead there is an output register in the IF stage
-  ID.io.instruction := IF.io.instruction
+//  ID.io.instruction := IF.io.instruction
 
   //---- EX <- IF -----//
-  EX.io.BranchAddressIn := IF.io.PC
+//  EX.io.BranchAddressIn := IF.io.PC
 
   //---- EX <- ID -----//
-  EX.io.instruction := IF.io.instruction // No register in ID stage anyways, so this is valid
-  EX.io.instrType := ID.io.instrType
-  EX.io.funct3 := ID.io.funct3
-  EX.io.funct7 := ID.io.funct7
-  EX.io.opcode := ID.io.opcode
-  EX.io.rs1 := ID.io.rs1
-  EX.io.rs2 := ID.io.rs2
-  EX.io.rd := ID.io.rd
+//  EX.io.instruction := IF.io.instruction // No register in ID stage anyways, so this is valid
+//  EX.io.instrType := ID.io.instrType
+//  EX.io.funct3 := ID.io.funct3
+//  EX.io.funct7 := ID.io.funct7
+//  EX.io.opcode := ID.io.opcode
+//  EX.io.rs1 := ID.io.rs1
+//  EX.io.rs2 := ID.io.rs2
+//  EX.io.rd := ID.io.rd
   //---- MEM <- EX  ----//
-  MEM.io.ALURes := EX.io.ALURes
-  MEM.io.address := EX.io.address
-  MEM.io.dataWriteMem := EX.io.dataWriteMem
-  MEM.io.memWriteEnable := EX.io.MemWriteEnable
-  MEM.io.registerWriteEnableIn := EX.io.RegWriteEnable
-  MEM.io.writeDataMux := EX.io.WriteDataMux
-  MEM.io.rdRegIn := EX.io.rdRegIn
-  MEM.io.BranchAddressIn := EX.io.BranchAddressOut
-  MEM.io.IOWriteEnable := EX.io.IOWriteEnable
-  MEM.io.readIsIO := EX.io.readIsIO
-  MEM.io.takeBranchIn := EX.io.takeBranchOut
-  MEM.io.memSel := EX.io.memSel
+//  MEM.io.ALURes := EX.io.ALURes
+//  MEM.io.address := EX.io.address
+//  MEM.io.dataWriteMem := EX.io.dataWriteMem
+//  MEM.io.memWriteEnable := EX.io.MemWriteEnable
+//  MEM.io.registerWriteEnableIn := EX.io.RegWriteEnable
+//  MEM.io.writeDataMux := EX.io.WriteDataMux
+//  MEM.io.rdRegIn := EX.io.rdRegIn
+//  MEM.io.BranchAddressIn := EX.io.BranchAddressOut
+//  MEM.io.IOWriteEnable := EX.io.IOWriteEnable
+//  MEM.io.readIsIO := EX.io.readIsIO
+//  MEM.io.takeBranchIn := EX.io.takeBranchOut
+//  MEM.io.memSel := EX.io.memSel
   //----  EX <- MEM ----//
-  EX.io.writeData := MEM.io.writeData
-  EX.io.writeEnable := MEM.io.registerWriteEnableOut
-  EX.io.rdReg := MEM.io.rdRegOut
+//  EX.io.writeData := MEM.io.writeData
+//  EX.io.writeEnable := MEM.io.registerWriteEnableOut
+//  EX.io.rdReg := MEM.io.rdRegOut
 
   //----- IF <- MEM ------//
-  IF.io.BranchAddress := MEM.io.BranchAddressOut
-  IF.io.EnableJump := MEM.io.takeBranchOut
+//  IF.io.BranchAddress := MEM.io.BranchAddressOut
+//  IF.io.EnableJump := MEM.io.takeBranchOut
 
   //---MEMORY MAPPED IO---//
   io.LEDs := MEM.io.leds
 
+  IFD.io <> EX.io
+  IFD.io <> MEM.io
+  EX.io <> MEM.io
+  EX.io <> WB.io
+  MEM.io <> WB.io
 
 }
 
